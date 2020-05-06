fbf86a63f178bed7334045872e580c91fa41b59d
Burk Hufnagel <304328+BurkHufnagel@example.com>
2020-05-06 18:05:24 -0400

    Fixed typo

--- src/test/groovy/com/dbs/VendingMachineSpec.groovy
+++ src/test/groovy/com/dbs/VendingMachineSpec.groovy
@@ -3,12 +3,12 @@ package com.dbs
 import spock.lang.Specification
 
 
-class VendingMachingSpec extends Specification {
+class VendingMachineSpec extends Specification {
 
     // A Spock version of the JUnit test in VendingMachineTest.java
     def "The message returned by the display should be the same as the message sent to the dislpay"() {
         given: "an instance of VendingMachine"
-            def machine = new VendingMachine();
+            def machine = new VendingMachineBase();
 
         and: "a message is sent to the display"
             def expectedMessage = "Smoke test"
